ca9295e682d4046e7bbdd41e7d08c86446e7adce
nick-hansen-sd <nhansen@csumb.edu>
2026-03-18 18:51:54 -0700

    Character controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0f, 0f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1f;
+        }
+
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
