e575781b721178e2343d1b58acd6c9709a7ab7a5
nick-hansen-sd <nhansen@csumb.edu>
2026-04-08 20:08:26 -0700

    Game start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,6 +39,11 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
     {
+        if (!KitchenGameManager.Instance.IsGamePlaying())
+        {
+            return;
+        }
+        
         if (selectedCounter != null)
         {
             selectedCounter.InteractAlternate(this);
@@ -47,6 +52,11 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteraction(object sender, System.EventArgs e)
     {
+        if (!KitchenGameManager.Instance.IsGamePlaying())
+        {
+            return;
+        }
+
         if (selectedCounter != null)
         {
             selectedCounter.Interact(this);
